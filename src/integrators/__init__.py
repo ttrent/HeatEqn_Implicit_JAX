5d5651a6e7a6785002190188c6c4850fed89200e
ttrent <tyler.kkh.trent@gmail.com>
2026-08-13 20:22:59 +0000

    add integrator registry

--- src/integrators/__init__.py
+++ src/integrators/__init__.py
@@ -3,7 +3,13 @@
 from integrators.backward_euler import backward_euler_residual
 from integrators.implicit_midpoint import implicit_midpoint_residual
 
+INTEGRATORS = {
+    "backward_euler": backward_euler_residual,
+    "implicit_midpoint": implicit_midpoint_residual,
+}
+
 __all__ = [
+    "INTEGRATORS",
     "backward_euler_residual",
     "implicit_midpoint_residual",
 ]
